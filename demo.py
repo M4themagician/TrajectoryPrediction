584cb3f95c52c4988cdcd1492ac868df044aa274
Lennart Evers <lennart.evers@topas.tech>
2024-01-15 12:54:27 +0100

    nicer plotting

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ import torch
 
 def demo():
     batch_size = 1
-    num_plots = 10
+    num_plots = 5
     print_n = 500 * batch_size
     input_steps = 150
     forecast_steps = 30
@@ -15,9 +15,13 @@ def demo():
     dataset = SingleTrackDataset(input_steps, forecast_steps, print_n)
     net = TrajectoryNetwork(input_dim=8, state_dim=6, input_timesteps=input_steps, forecast_steps=forecast_steps)
     net = nn.DataParallel(net)
-    net.load_state_dict(torch.load("weights_.pth"))
+    net.load_state_dict(torch.load("weights.pth"))
     net = net.module.cuda()
-    for _ in range(num_plots):
+    fig, ax = plt.subplots(5, 5, layout='compressed')
+
+    for i in range(num_plots*num_plots):
+        j = i % num_plots
+        k = i // num_plots
         x, y = dataset.__getitem__(0)
         x = x.cuda().unsqueeze(0)
         y = y.cuda().unsqueeze(0)
@@ -27,11 +31,11 @@ def demo():
         future_predicted = torch.unflatten(prediction[0].detach().squeeze(0), dim=0, sizes=(6, forecast_steps)).cpu().numpy()
         input_x = np.cumsum(input_trajectory[0, :])
         input_y = np.cumsum(input_trajectory[1, :])
-        plt.plot(input_x, input_y, label="input")
-        plt.plot(np.cumsum(future_gt[0, :]) + input_x[-1], np.cumsum(future_gt[1, :]) + input_y[-1], label="gt")
-        plt.plot(np.cumsum(future_predicted[0, :]) + input_x[-1], np.cumsum(future_predicted[1, :]) + input_y[-1], label="prediction")
-        plt.legend()
-        plt.show()
+        ax[j, k].plot(input_x, input_y, label="input")
+        ax[j, k].plot(np.cumsum(future_gt[0, :]) + input_x[-1], np.cumsum(future_gt[1, :]) + input_y[-1], label="gt")
+        ax[j, k].plot(np.cumsum(future_predicted[0, :]) + input_x[-1], np.cumsum(future_predicted[1, :]) + input_y[-1], label="prediction")
+        ax[j, k].legend()
+    plt.show()
 
 
 def model_predictive_simulation():
